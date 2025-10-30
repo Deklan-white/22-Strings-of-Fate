4c8dce42ea5bbdf87f407527557bfd774d4eb966
deklan-w <deklan.white@allsaintshigh.org>
2025-10-30 10:44:32 -0500

    Lab dealing with strings

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,25 @@
 public class Runner {
     public static void main(String[] args){
+        int int1 = 4;
+        double avg1 = StringySpells.aveLen("dog","aplus");
+        System.out.println("Average 1: " + avg1);
+        double avg2 = StringySpells.aveLen("ap","pluscat");
+        System.out.println("Average 2: " + avg2);
+
+        String middle1 = StringySpells.middleFinder("catgiraffe");
+        System.out.println("Middle 1: " + middle1);
+
+        String abeta = StringySpells.alphaOmega("aplus", "dog");
+        System.out.println("Alpha: " + abeta);
+
+        String flipper = StringySpells.flipper("compsci","supercool");
+        System.out.println("Flipper: " + flipper);
+
+        String abg = StringySpells.subtractive("lolipop", int1);
+        System.out.println("subtractive:" + abg);
+
+
+
 
     }
 }
--- src/main/java/StringySpells.java
+++ src/main/java/StringySpells.java
@@ -2,15 +2,39 @@
 
 public class StringySpells {
 
-    // middleFinder
+    public static String middleFinder(String i) {
+        int len1 = i.length();
+        int middle = len1 / 2;
+        return i.substring(middle,middle +1 );
+    }
 
-    // alphaOmega
+    public static String alphaOmega(String a, String b) {
+        String alpha = a.substring(0,1);
+        int len = b.length();
 
-    // subtractive
+        String beta = b.substring(len - 1, len);
+        return alpha + beta;
+    }
 
-    // flipper
+    public static String subtractive(String a, int b) {
+        return a.substring(0,b) + a.substring(b+1);
+    }
 
-    // aveLen
+    public static String flipper(String one, String two) {
+        String first = one.substring(0);
+        String second = two.substring(0,1);
+        return second + first;
+
+
+    }
+
+
+    public static double aveLen(String s1, String s2) {
+        double len1 = s1.length();
+        double len2 = s2.length();
+        return (len1 + len2) / 2;
+    }
+
 
 
 }//end of class
